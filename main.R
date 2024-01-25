26d3fcb4e1ef3089f79b0ef811f5b744bb6ede50
ReptilianEye <piotr.rzadkowski@outlook.com>
2024-01-25 09:19:56 +0100

    ms with mean calls

--- main.R
+++ main.R
@@ -17,7 +17,6 @@ find_min_prs <- function(f, n, range) {
     min_v <- Inf
     for (i in 1:n) {
         v <- c(runif(R, range[1], range[2]))
-        # min_v <- min(min_v, do.call(f, c(as.list(v))))
         min_v <- min(min_v, f(v))
     }
     return(min_v)
@@ -29,7 +28,7 @@ prs_mean <- function(f, size, n, range) {
 
 
 f <- ackley_R2 # function to evaluate
-size <- 20 # number of samples
+size <- 50 # number of samples
 n <- 1000 # number of iterations
 range <- ackley_range # range of values for parameters
 prs_mean(f, size, n, range)
@@ -38,21 +37,36 @@ prs_mean(f, size, n, range)
 find_min_ms <- function(f, n, range) {
     R <- getParamLengths(getParamSet(f))
     points <- matrix(0, nrow = n, ncol = R)
-    for (i in 1:R) {
-        points[, i] <- runif(n, range[1], range[2])
+    for (i in 1:n) {
+        points[i, ] <- runif(R, range[1], range[2])
     }
     min_v <- Inf
+    lower <- replicate(R, range[1])
+    upper <- replicate(R, range[2])
+    calls <- 0
     for (i in 1:n) {
-        min_v <- min(min_v, optim(points[i, ], f)$value)
+        result <- optim(points[i, ], f, lower = lower, upper = upper, method = "L-BFGS-B")
+        min_v <- min(min_v, result$value)
+        calls <- calls + result$counts[1]
+        # min_v <- min(min_v, optim(points[i, ], f)$value, lower = lower, upper = upper, method = "L-BFGS-B")
     }
-    return(min_v)
+    return(c(min_v, calls))
 }
 ms_mean <- function(f, size, n, range) {
-    y <- replicate(size, find_min_ms(f, n, range))
-    return(mean(y))
+    result <- matrix(0, nrow = size, ncol = 2)
+    for (i in 1:size) {
+        result[i, ] <- find_min_ms(f, n, range)
+    }
+    # y <- replicate(size, matrix(find_min_ms(f, n, range)))
+    y <- result[, 1]
+    calls <- result[, 2]
+    return(c(mean(y), mean(calls)))
 }
 f <- ackley_R2
-n <- 10
-size <- 20
+n <- 100
+size <- 50
 range <- ackley_range
-ms_mean(f, size, n, range)
+res <- ms_mean(f, size, n, range)
+min_v <- res[1]
+calls <- res[2]
+prs_mean(ackley_R2, 50, calls, ackley_range)
